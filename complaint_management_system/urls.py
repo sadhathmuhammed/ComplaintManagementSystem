fd11105bf22606c740f67c4a71e07cdb91c1270e
Muhammed Sadhath <muhammed.sadhath@scrapehero.com>
2025-07-27 23:58:00 +0530

    heading color change

--- complaint_management_system/urls.py
+++ complaint_management_system/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('complaint_management.urls') )
+    path('login/', include('complaint_management.urls') )
 ]
